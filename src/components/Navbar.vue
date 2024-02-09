a6f8e02a2bae48f34e4458a1d9ed3c8509ea2990
Thanakorn <sakura__kung@hotmail.com>
2024-02-09 16:09:40 +0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <div class="testnav">Navbar</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .testnav{
+        color: #bb0c0c;
+    }
+</style>
